353c07c88d63ab40f970acf4c1a6b9b267a06ead
tenutz <tentenacy@gmail.com>
2021-12-13 01:24:03 +0900

    test: signup and that request validation

--- src/main/java/com/untilled/roadcapture/api/dto/user/SignupRequest.java
+++ src/main/java/com/untilled/roadcapture/api/dto/user/SignupRequest.java
@@ -3,6 +3,7 @@ package com.untilled.roadcapture.api.dto.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.*;
 
@@ -11,7 +12,8 @@ import javax.validation.constraints.*;
 public class SignupRequest {
     @Email
     private String email;
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
+    @Length(min = 8, max = 64)
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$")
     private String password;
     @Size(min = 2, max = 12)
     private String username;
--- src/test/java/com/untilled/roadcapture/api/controller/UserApiControllerTest.java
+++ src/test/java/com/untilled/roadcapture/api/controller/UserApiControllerTest.java
@@ -3,31 +3,23 @@ package com.untilled.roadcapture.api.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.untilled.roadcapture.api.dto.user.SignupRequest;
 import com.untilled.roadcapture.domain.user.UserService;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -47,10 +39,32 @@ class UserApiControllerTest {
     @DisplayName("회원가입")
     class Signup {
         @Test
-        @DisplayName("이메일 형식 맞지 않으면 실패")
-        void When_EmailTypeIsMismatched_Expect_SignupToFail() throws Exception {
+        @DisplayName("성공")
+        void Success() throws Exception {
             //given
-            final SignupRequest signupRequest = new SignupRequest("test", "abdc1234", "tester");
+            final SignupRequest signupRequest = new SignupRequest("test@gmail.com", "abdc1234", "user");
+
+            //when
+            ResultActions result = mockMvc.perform(post("/users")
+                    .content(mapper.writeValueAsString(signupRequest))
+                    .contentType(MediaType.APPLICATION_JSON));
+
+            //then
+            result.andExpect(status().isCreated())
+                    .andDo(document("user-signup",
+                            requestFields(
+                                    fieldWithPath("email").description("사용자 이메일입니다. 이메일 형식이어야 합니다."),
+                                    fieldWithPath("password").description("사용자 비밀번호입니다. 영문 숫자 조합 최소 8자 이상에서 최대 64자 이하여야 합니다."),
+                                    fieldWithPath("username").description("사용자 이름입니다. 최소 2자 이상에서 최대 12자 이하여야 합니다.")
+                            )
+                    ));
+        }
+
+        @Test
+        @DisplayName("이메일 형식 맞지 않으면 실패")
+        void EmailTypeIsMismatched_Fail() throws Exception {
+            //given
+            final SignupRequest signupRequest = new SignupRequest("test", "abdc1234", "user");
 
             //when
             ResultActions result = mockMvc.perform(post("/users")
@@ -59,13 +73,88 @@ class UserApiControllerTest {
 
             //then
             result.andExpect(status().isBadRequest())
-                    .andDo(document("user-signup-email-mismatch",
-                            requestFields(
-                                    fieldWithPath("email").description("사용자 이메일입니다. 이메일 형식이어야 합니다."),
-                                    fieldWithPath("password").description("사용자 비밀번호입니다. 영문 숫자 조합 최소 8자 이상이어야 합니다."),
-                                    fieldWithPath("username").description("사용자 이름입니다. 최소 2자, 최대 12자입니다.")
-                            )
-                    ));
+                    .andDo(document("user-signup-email-mismatch"));
         }
+
+        @Test
+        @DisplayName("닉네임 길이가 2보다 짧으면 실패")
+        void UsernameSizeIsLessThan2_Fail() throws Exception {
+            //given
+            final SignupRequest signupRequest = new SignupRequest("test@gmail.com", "abdc1234", "u");
+
+            //when
+            ResultActions result = mockMvc.perform(post("/users")
+                    .content(mapper.writeValueAsString(signupRequest))
+                    .contentType(MediaType.APPLICATION_JSON));
+
+            //then
+            result.andExpect(status().isBadRequest())
+                    .andDo(document("user-signup-username-shorter"));
+        }
+
+        @Test
+        @DisplayName("닉네임 길이가 12보다 길면 실패")
+        void UsernameSizeIsGreaterThan12_Fail() throws Exception {
+            //given
+            final SignupRequest signupRequest = new SignupRequest("test@gmail.com", "abdc1234", "usernameusern");
+
+            //when
+            ResultActions result = mockMvc.perform(post("/users")
+                    .content(mapper.writeValueAsString(signupRequest))
+                    .contentType(MediaType.APPLICATION_JSON));
+
+            //then
+            result.andExpect(status().isBadRequest())
+                    .andDo(document("user-signup-username-longer"));
+        }
+
+        @Test
+        @DisplayName("비밀번호 형식 맞지 않으면 실패")
+        void PasswordTypeIsMismatch_Fail() throws Exception {
+            //given
+            final SignupRequest signupRequest = new SignupRequest("test@gmail.com", "abcdefgh", "username");
+
+            //when
+            ResultActions result = mockMvc.perform(post("/users")
+                    .content(mapper.writeValueAsString(signupRequest))
+                    .contentType(MediaType.APPLICATION_JSON));
+
+            //then
+            result.andExpect(status().isBadRequest())
+                    .andDo(document("user-signup-password-mismatch"));
+        }
+
+        @Test
+        @DisplayName("비밀번호 길이가 8보다 짧으면 실패")
+        void PasswordSizeIsLessThan8_Fail() throws Exception {
+            //given
+            final SignupRequest signupRequest = new SignupRequest("test@gmail.com", "abcd123", "username");
+
+            //when
+            ResultActions result = mockMvc.perform(post("/users")
+                    .content(mapper.writeValueAsString(signupRequest))
+                    .contentType(MediaType.APPLICATION_JSON));
+
+            //then
+            result.andExpect(status().isBadRequest())
+                    .andDo(document("user-signup-password-shorter"));
+        }
+
+        @Test
+        @DisplayName("비밀번호 길이가 64보다 길면 실패")
+        void PasswordSizeIsGreaterThan64_Fail() throws Exception {
+            //given
+            final SignupRequest signupRequest = new SignupRequest("test@gmail.com", "abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789", "username");
+
+            //when
+            ResultActions result = mockMvc.perform(post("/users")
+                    .content(mapper.writeValueAsString(signupRequest))
+                    .contentType(MediaType.APPLICATION_JSON));
+
+            //then
+            result.andExpect(status().isBadRequest())
+                    .andDo(document("user-signup-password-longer"));
+        }
+
     }
 }
